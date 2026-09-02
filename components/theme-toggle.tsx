610f9fb1c537ee155af1bf5797a966ef175a0b9d
hamid842 <h_mohammadi842@yahoo.com>
2026-09-02 14:04:47 +0330

    [FIX] theme toggle updated

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -2,22 +2,9 @@
 
 import { useTheme } from "@wrksz/themes/client";
 import { Moon, Sun } from "lucide-react";
-import { useSyncExternalStore } from "react";
-
-const emptySubscribe = () => () => {};
 
 export function ThemeToggle() {
   const { resolvedTheme, setTheme } = useTheme();
-
-  const mounted = useSyncExternalStore(
-    emptySubscribe,
-    () => true,
-    () => false,
-  );
-
-  if (!mounted) {
-    return null;
-  }
 
   const isDark = resolvedTheme === "dark";
 
